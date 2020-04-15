7f8ac02e9b93f74c4d6d1e9517f68df1d2f43599
sthomas20 <sthomas8@iastate,edugit config --global user.email sthomas8@iastate.edu>
2020-04-14 21:06:52 -0500

    Finished Q5-6, began Q7/8

--- individual2.R
+++ individual2.R
@@ -41,7 +41,7 @@ combined$SEX <- as.factor(combined$SEX)
 
 combined %>%
   filter(SEX == 1 | SEX ==2) %>%
-  filter(HOUR < 24) %>%
+  filter(HOUR <= 24) %>%
   ggplot(combined, mapping =aes(x= HOUR, fill=wday(DAY_WEEK, label = TRUE))) + geom_bar() + facet_wrap(~SEX) + labs(fill = "Day of Week") + xlab('Hour of Day') + ylab('Number of accidents') + ggtitle('Accidents by Day of week, hour, and sex')
 
 
@@ -49,3 +49,39 @@ combined %>%
   filter(SEX == 1 | SEX == 2) %>%
   filter(HOUR < 24) %>%
   ggplot(combined, mapping =aes(x= HOUR, fill=SEX)) + geom_bar() + facet_wrap(~wday(DAY_WEEK, label = TRUE)) + scale_fill_discrete(name = "SEX", labels = c('Male', 'Female')) + xlab('Hour of Day') + ylab('Number of accidents') + ggtitle('Accidents by Day of week, hour, and sex')
+
+#PART THREE:
+
+#Question 7/8
+library("readxl")
+
+US_GLC<- readxl::read_xlsx('FRPP GLC - United StatesFeb132020.xlsx')
+#US_territory<- read_excel('FRPP_GLC_US_Territories12419.xlsx')
+
+US_GLC <- US_GLC %>%
+  select('State Name', STATE = 'State Code', COUNTY = 'County Code', 'County Name') %>%
+  unique()
+
+US_GLC$STATE <- as.integer(US_GLC$STATE)
+US_GLC$COUNTY <- as.integer(US_GLC$STATE)
+US_GLC$`State Name` <- tolower(US_GLC$`State Name`)
+US_GLC$`County Name` <- tolower(US_GLC$`County Name`)
+
+join<- acc %>%
+  select(STATE, ST_CASE, COUNTY) %>%
+  group_by(STATE, COUNTY) %>%
+  summarize(total_accidents= n()) %>%
+  left_join(US_GLC, by = c('STATE', 'COUNTY'))
+
+county<- map_data('county')
+
+county <- county %>%
+  mutate(`State Name` = region, `County Name` = subregion) %>%
+  select(`State Name`, `County Name`, long, lat, group, order)
+
+join <- join %>%
+  full_join(county, by = c('State Name', 'County Name'))
+
+join<- join[order(join$order), ]
+
+ggplot(join, aes(long, lat)) + geom_polygon(aes(group = group, fill = total_accidents)) 
